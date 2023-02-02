375c4f316dbe410281c689fd91b796097128c3d1
Vithusha Ravirajan <vithusharavirajan@Vithus-MBP.local>
2023-02-02 15:22:37 -0500

    dynamic content and footer

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <div className="Footer">
+        <hr className="Line"/>
+	    <small>Mitchell Hudson copyright 2019</small>
+    </div>
+  )
+}
+
+export default Footer
