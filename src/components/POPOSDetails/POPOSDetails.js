4d8bd9dc82caf50790733ac17036cb0fee41cbe4
Vithusha Ravirajan <vithusharavirajan@Vithus-MBP.local>
2023-02-05 00:33:58 -0500

    completed project

---
+++ src/components/POPOSDetails/POPOSDetails.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useParams } from 'react-router'
+import data from '../../sfpopos-data.js'
+import './POPOSDetails.css'
+import POPOSFeatureList from '../POPOSFeatures/POPOSFeatureList'
+
+function POPOSDetails(props) {
+  const params = useParams()
+  const { id } = params // Location index
+  const { images, title, desc, hours, features, geo } = data[id]
+
+  return (
+<div className="POPOSDetails">
+  <div className="POPOSDetails-image">
+    <img src={`${process.env.PUBLIC_URL}images/${images[0]}`} alt={title}/>
+  </div>
+  <div className="POPOSDetails-info">
+    <h1 className="POPOSDetails-title">{ title }</h1>
+    <p className="POPOSDetails-desc">{ desc }</p>
+    <p className="POPOSDetails-hours">{ hours }</p>
+    <POPOSFeatureList features={features}/>
+    <p className="POPOSDetails-geo">Coordinates: { geo.lat } { geo.lon }</p>
+  </div>
+</div>
+  )
+}
+
+export default POPOSDetails
---
+++ src/components/POPOSList/POPOSList.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import POPOSSpace from '../POPOSSpace/POPOSSpace';
+import './POPOSList.css';
+import data from '../../sfpopos-data.js'
+import { useState } from 'react'
+
+
+function POPOSList() {
+  const [ query, setQuery ] = useState('');
+  const spaces = data.filter((obj) => {
+    // true if query is in title
+    const inTitle = obj.title.toLowerCase().includes(query.toLowerCase())
+    // true if query is in address
+    const inAddress = obj.address.toLowerCase().includes(query.toLowerCase())
+    // return true if either is true
+    return inTitle || inAddress
+  })
+  .map((obj) => {
+    const { id, title, address, images, hours, features} = obj
+  // const spaces = data.map(({ title, address, images, hours }, i) => {
+    return (
+      <POPOSSpace
+        id={id}
+        key={`${title}-${id}`}
+        name={title}
+        address={address}
+        image={images[0]}
+        hours={hours}
+        features ={features}
+      />
+    )
+  })
+    
+
+
+  return (
+    <div>
+      <div className="POPOSListForm">
+      <form>
+				<input
+					value={query}
+					placeholder="search"
+					onChange={(e) => setQuery(e.target.value)}
+				/>
+				<button type="submit">Submit</button>
+			</form>
+      </div>
+      <div className="POPOSList">
+			{spaces}
+    </div>
+      <div className="Footer">
+          <hr className="Line"/>
+        <small>Mitchell Hudson copyright 2019</small>
+      </div>
+    </div>
+  )
+}
+  
+  export default POPOSList
