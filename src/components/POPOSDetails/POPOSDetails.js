ab67fe0b94c6ef348f9e8c818292a3258cc5e679
Vithusha Ravirajan <vithusharavirajan@Vithus-MBP.local>
2023-02-05 01:38:18 -0500

    made some updated to random button

--- src/components/POPOSDetails/POPOSDetails.js
+++ src/components/POPOSDetails/POPOSDetails.js
@@ -3,6 +3,7 @@ import { useParams } from 'react-router'
 import data from '../../sfpopos-data.js'
 import './POPOSDetails.css'
 import POPOSFeatureList from '../POPOSFeatures/POPOSFeatureList'
+import RandomSpace from '../RandomSpace/RandomSpace'
 
 function POPOSDetails(props) {
   const params = useParams()
@@ -10,17 +11,22 @@ function POPOSDetails(props) {
   const { images, title, desc, hours, features, geo } = data[id]
 
   return (
-<div className="POPOSDetails">
-  <div className="POPOSDetails-image">
-    <img src={`${process.env.PUBLIC_URL}/images/${images[0]}`} alt={title}/>
-  </div>
-  <div className="POPOSDetails-info">
-    <h1 className="POPOSDetails-title">{ title }</h1>
-    <p className="POPOSDetails-desc">{ desc }</p>
-    <p className="POPOSDetails-hours">{ hours }</p>
-    <POPOSFeatureList features={features}/>
-    <p className="POPOSDetails-geo">Coordinates: { geo.lat } { geo.lon }</p>
-  </div>
+    <div>
+      <div className='randomspace-insert'>
+      <RandomSpace />
+      </div>
+      <div className="POPOSDetails">
+        <div className="POPOSDetails-image">
+          <img src={`${process.env.PUBLIC_URL}/images/${images[0]}`} alt={title}/>
+        </div>
+        <div className="POPOSDetails-info">
+          <h1 className="POPOSDetails-title">{ title }</h1>
+          <p className="POPOSDetails-desc">{ desc }</p>
+          <p className="POPOSDetails-hours">{ hours }</p>
+          <POPOSFeatureList features={features}/>
+          <p className="POPOSDetails-geo">Coordinates: { geo.lat } { geo.lon }</p>
+        </div>
+      </div>
 </div>
   )
 }
